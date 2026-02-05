477e1029903027f828e2f0c445c83c21266f11d0
melchinov_pv <melchinov_pv@albank.ru>
2026-02-06 08:33:37 +0900

    Fix Add new files

---
+++ Closure/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Closure.Classes;
+
+var engine = new ServiceProviderEngine();
+var firstFunc = engine.RealizeService(new ServiceCallSite{Name = "план-схема объекта"});
+firstFunc(new ServiceProviderEngineScope{Name = "Первый скоуп"});
+firstFunc(new ServiceProviderEngineScope{Name = "Второй скоуп"});
+
+
+
+
---
+++ VisitorSwitch/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1. Создаем отдельные товары
+
+using VisitorSwitch.Classes;
+
+var iPhone = new Node { Kind = ItemKind.SingleProduct, Price = 1000m };
+var cases = new Node { Kind = ItemKind.SingleProduct, Price = 50m };
+var cable = new Node { Kind = ItemKind.SingleProduct, Price = 25m };
+
+// 2. Создаем коробку с аксессуарами
+var accessoriesBox = new Node 
+{ 
+    Kind = ItemKind.Box, 
+    Children = new List<Node> { cases, cable } 
+};
+
+// 3. Создаем главную коробку (товар + коробка аксессуаров)
+var mainOrder = new Node
+{
+    Kind = ItemKind.Box,
+    Children = new List<Node> { iPhone, accessoriesBox }
+};
+
+Console.WriteLine($"Общая сумма товаров в коробке: ${new PriceVisitor().VisitPrice(mainOrder)}");
